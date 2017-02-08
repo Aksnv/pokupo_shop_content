7623745d33fdf6ae46dc6ea74fd2308422a963b9
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-02-08 23:37:54 +0300

    Добавлены стили и скрипты для выпадающего блока корзины

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,10 @@ $(".page-header--bottom__platform-button").click(function() {
     $(".page-header--bottom__platform-button::before").css("transform", "rotate(180deg)");
     $(".page-header--top").show();
     $(".page-header--bottom").css("marginTop", 88);
+    $(".page-header--bottom__platform").css("top", "83px");
     $(".page-header").css("borderTop", "none");
     $(".page-header").addClass("page-header--open");
+    $(".page-header--bottom").addClass("page-header--bottom--open");
   } else {
     $(this).removeClass("page-header--bottom__platform-button--close");
     $(this).addClass("page-header--bottom__platform-button--open");
@@ -19,8 +21,10 @@ $(".page-header--bottom__platform-button").click(function() {
     $(".page-header--bottom__platform-button").css("paddingLeft", "20px");
     $(".page-header--top").hide();
     $(".page-header--bottom").css("marginTop", 0);
+    $(".page-header--bottom__platform").css("top", 0);
     $(".page-header").css("borderTop", "5px solid #fff");
     $(".page-header").removeClass("page-header--open");
+    $(".page-header--bottom").removeClass("page-header--bottom--open");
   }
 });
 
@@ -46,7 +50,7 @@ $(".banner-gallery__paginator-item").click(function() {
 });
 
 
-/* Меню кабинета пользователя */
+/* Выпадающее меню кабинета пользователя */
 
 $(".page-header--bottom__login").click(function() {
   $(".page-header--bottom__login-inner").toggleClass("page-header--bottom__login-inner--close");
@@ -66,5 +70,30 @@ $(".page-header--bottom__login").mouseout(function() {
     $(".page-header--bottom__login-logo").css("backgroundPosition", "-7px -7px");
   } else {
     $(".page-header--bottom__login-logo").css("backgroundPosition", "-120px -95px");
+  }
+});
+
+
+/* Выпадающая корзина */
+
+$(".page-header--bottom__cart").click(function() {
+  $(".page-header--bottom__cart-inner").toggleClass("page-header--bottom__cart-inner--close");
+  $(".page-header--bottom__cart-logo").toggleClass("page-header--bottom__cart-logo--open");
+  if ($(".page-header--bottom__cart-inner").hasClass("page-header--bottom__cart-inner--close")) {
+    $(".page-header--bottom__cart-logo").css("backgroundPosition", "-32px -7px");
+  } else {
+    $(".page-header--bottom__cart-logo").css("backgroundPosition", "-151px -8px");
+  }
+});
+
+$(".page-header--bottom__cart").mouseover(function() {
+  $(".page-header--bottom__cart-logo").css("backgroundPosition", "-151px -8px");
+});
+
+$(".page-header--bottom__cart").mouseout(function() {
+  if ($(".page-header--bottom__cart-inner").hasClass("page-header--bottom__cart-inner--close")) {
+    $(".page-header--bottom__cart-logo").css("backgroundPosition", "-32px -7px");
+  } else {
+    $(".page-header--bottom__cart-logo").css("backgroundPosition", "-151px -8px");
   }
 });
